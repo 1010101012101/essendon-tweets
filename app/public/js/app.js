f4778ea799b307a7a8b4375ce58d2ca89da21a68
healsjnr <healsjnr@gmail.com>
2014-03-14 13:56:22 +1000

    minor updates

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -3,3 +3,17 @@ $(document).ready(function() {
     singleItem : true
   });
 });
+
+$(document).keydown(function(e) {
+  var owl = $('#owl-demo');
+  switch(e.which) {
+    case 37:
+      owl.trigger('owl.prev');
+      break;
+    case 39:
+      owl.trigger('owl.next');
+      break;
+    default: return;
+  }
+  e.preventDefault();
+});
